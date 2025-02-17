4b77d37b97368aab8a569dcb8b23b79eaf089741
Thanh Vu Le <tvl@eiva.com>
2025-02-17 15:52:50 +0100

    updated CMake configuration for Fast DDS examples and added HelloWorld example with Python bindings

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,7 @@ class EivaFastDDSSwig(ConanFile):
             del self.options.fPIC
 
     def layout(self):
-        cmake_layout(self, src_folder="fastdds_csharp")
+        cmake_layout(self, src_folder="fastdds_csharp_examples")
 
     def requirements(self):
        self.requires("swig/4.2.1")
