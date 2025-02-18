b74c780c405716dbb9878e545f71c1a5cc821f0f
Thanh Vu Le <tvl@eiva.com>
2025-02-18 14:40:43 +0100

    refactor: update CMake layout for Fast DDS C# examples and extend Subscriber class with create_datareader_topic method

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,7 @@ class EivaFastDDSSwig(ConanFile):
             del self.options.fPIC
 
     def layout(self):
-        cmake_layout(self, src_folder="fastdds_csharp_examples")
+        cmake_layout(self, src_folder="fastdds_csharp")
 
     def requirements(self):
        self.requires("swig/4.2.1")
