8ea64237818d35fb7c21130e74af9db092b48a2d
Thanh Vu Le <tvl@eiva.com>
2025-03-10 10:03:34 +0100

    fix: update SWIG wrapper include paths in build script

--- fastdds_javascript/build_javascript.bat
+++ fastdds_javascript/build_javascript.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling SWIG wrapper 
-C:/Users/tvl/.conan2/p/b/swig4fe21ed3bac24/p/bin/swig.exe -IC:/Users/tvl/.conan2/p/eiva-7e4aa1c6ead66/p/include -IC:/Users/tvl/.conan2/p/eiva-1aa07c1c70d2b/p/include -c++ -javascript -node src/swig/fastdds.i
+C:/Users/tvl/.conan2/p/b/swig4fe21ed3bac24/p/bin/swig.exe -IC:/Users/tvl/.conan2/p/b/eiva-cf579c7c05774/p/include -IC:/Users/tvl/.conan2/p/eiva-1aa07c1c70d2b/p/include -c++ -javascript -node src/swig/fastdds.i
 
 echo Building node-gyp bundle
 npm run build
