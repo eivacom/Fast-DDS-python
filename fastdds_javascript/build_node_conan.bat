cdc6330ffa71d1125d6c3c250f23ddfabaeb10d8
Thanh Vu Le <tvl@eiva.com>
2025-04-01 13:56:17 +0200

    fix: update build scripts to use 'call' for SWIG and npm commands

--- fastdds_javascript/build_node_conan.bat
+++ fastdds_javascript/build_node_conan.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling SWIG wrapper 
-C:/Users/tvl/.conan2/p/b/swig4fe21ed3bac24/p/bin/swig.exe -IC:/Users/tvl/.conan2/p/b/eiva-cf579c7c05774/p/include -IC:/Users/tvl/.conan2/p/eiva-1aa07c1c70d2b/p/include -c++ -javascript -node src/swig/fastdds.i
+call C:/Users/tvl/.conan2/p/b/swig4fe21ed3bac24/p/bin/swig.exe -IC:/Users/tvl/.conan2/p/b/eiva-cf579c7c05774/p/include -IC:/Users/tvl/.conan2/p/eiva-1aa07c1c70d2b/p/include -c++ -javascript -node src/swig/fastdds.i
 
 echo Building node-gyp bundle
-npm run build
+call npm run build
